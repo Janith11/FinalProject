2d8634c79606ef0a9877dc4ac4afb698b6f67f92
dmAnushka <78608402+dmAnushka@users.noreply.github.com>
2021-03-29 19:56:06 -0700

    added instructors interface

---
+++ instructor.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><!--aaaaa--></title>
+	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+	<nav class="navbar navbar-expand-sm bg-light">
+		<ul class="navbar-nav">
+			<li class="nav-item">
+				<a class="nav-link" href="#">logo</a>
+
+			</li>
+			
+		</ul>
+
+	</nav>
+
+		<div class="row ">
+			<div class="col-12">
+			<h1 class="text-center">INSTRUCTORS</h1>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-12 col-lg-6" >
+				<p class="text-center lead">Instructors name1</p>
+				<img src="pic1.jpg" class="mx-auto d-block img-fluid" alt="">
+				<ul>
+					<li>post :xxxxxxxxxxxxxxxxxx</li>
+					<li>Vehical type:xxxxxxxxxx</li>
+					<li>contac number:xxxxxxxxx</li>
+
+				</ul>
+			</div> 			
+
+			<div class="col-12 col-lg-6">
+				<p class="text-center lead">Instructors name2</p>
+				<img src="pic2.jpg" class="mx-auto d-block img-fluid" 	alt="">
+				<ul>
+					<li>post :xxxxxxxxxxxxxxxxxx</li>
+					<li>Vehical type:xxxxxxxxxx</li>
+					<li>contac number:xxxxxxxxx</li>
+
+				</ul>
+			</div> 			
+
+			<div class="col-12 col-lg-6">
+				<p class="text-center lead">Instructors name3</p>
+				<img src="pic3.jpg" class="mx-auto d-block img-fluid" alt="">
+				<ul>
+					<li>post :xxxxxxxxxxxxxxxxxx</li>
+					<li>Vehical type:xxxxxxxxxx</li>
+					<li>contac number:xxxxxxxxx</li>
+
+				</ul>
+			</div> 			
+
+			<div class="col-12 col-lg-6">
+				<p class="text-center lead">Instructors name4</p>
+				<img src="pic4.jpg" class="mx-auto d-block img-fluid" alt="">
+				<ul>
+					<li>post :xxxxxxxxxxxxxxxxxx</li>
+					<li>Vehical type:xxxxxxxxxx</li>
+					<li>contac number:xxxxxxxxx</li>
+
+				</ul>
+			</div> 			
+
+
+		</div>
+
+	
+
+</body>
+</html>
